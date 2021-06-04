d6b11a672a66bbb052c462dcf8185fa28faba60b
Saeed Bahatheq <saeed.bahatheq@gmail.com>
2021-06-03 20:00:31 -0400

    Implement shifter

--- app.js
+++ app.js
@@ -28,108 +28,128 @@ const port = 3000;
 
 
 function parseTime(str) {
-    var elems = str.replace(',','.').split(':').map(parseFloat);
-    // console.log('elems are ', elems);
-    return elems[0] * 3600 + elems[1] * 60 + elems[2];
+  var elems = str.replace(',', '.').split(':').map(parseFloat);
+  return elems[0] * 3600 + elems[1] * 60 + elems[2];
 }
 
 function stringifyTime(t) {
-    var h = Math.floor(t/3600);
-    var m = Math.floor((t - h * 3600) / 60);
-    var s = (t - h * 3600 - m * 60).toFixed(3);
-    return [h,m,s].map(padz).join(':').replace('.',',');
+  var h = Math.floor(t / 3600);
+  var m = Math.floor((t - h * 3600) / 60);
+  var s = (t - h * 3600 - m * 60).toFixed(3);
+  return [h, m, s].map(padz).join(':').replace('.', ',');
 }
 
-function parseSpec(spec) {
-    var positive = ~spec.indexOf('+');
-    var timepos = spec.split(/[-+]/);
-    var t = parseTime(timepos[0]),
-        shift = parseFloat(timepos[1]);
-    if (!positive) shift = 0 - shift;
-    return { at: t, shift: shift };
+
+function parseUserInput(spec) {
+  var [t_1, t_2] = [parseTime(spec.firstTimeStamp), parseTime(spec.secondTimeStamp)];
+  var [shift_1, shift_2] = [spec.firstTimeFix, spec.secondTimeFix]
+  return [{
+    at: t_1,
+    shift: parseFloat(shift_1, 10)
+  }, {
+    at: t_2,
+    shift: parseFloat(shift_2, 10)
+  }];
 }
 
-function padz(n) { return n >= 10 ? n : '0'+n; }
+function padz(n) {
+  return n >= 10 ? n : '0' + n;
+}
 
 function createShifter(specs) {
-    return function(pos) {
-       for (var k = 1; specs[k].at < pos; ++k);
-       var start = specs[k-1], end = specs[k];
-       var percent = (pos - start.at) / (end.at - start.at)
-       var shift = start.shift + percent * (end.shift - start.shift);
-       return (pos + shift) > 0 ? pos + shift : 0;
-    }
+  return function(pos) {
+    var start = specs[0],
+      end = specs[1];
+    // console.log('inside createShifter function are: ', start, end);
+    var slope = (end.shift - start.shift) / (end.at - start.at);
+    // console.log('this is slope: ', slope);
+    var shift = start.shift + (slope * (pos - start.at))
+    // console.log('this is shift: ', shift);
+    // console.log('this is pos: ', pos);
+    var output = pos + shift;
+    output = parseFloat(output)
+    // console.log('this is output: ', output);
+    return output;
+  }
 }
 
 function createWorkingFile(filename) {
   var sub = [];
   var test = '';
-
-  fs.readFile(__dirname + '/uploads/' + filename, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err)
+  sub.push(fs.readFileSync(__dirname + '/uploads/' + filename, 'utf8'));
+  var lines = sub.join('').split(/\r*\n/);
+  var subs = [];
+  var expect = 'new';
+  var last;
+  lines.forEach(function(l) {
+    if (expect == 'new') {
+      last = {
+        id: parseInt(l, 10),
+        text: ''
+      };
+      if (!isNaN(last.id)) expect = 'time';
+    } else if (expect == 'time') {
+      var twoTimes = l.split(/\s+-+>\s+/);
+      last.start = parseTime(twoTimes[0]);
+      last.end = parseTime(twoTimes[1]);
+      expect = 'text_end';
+    } else {
+      if (l.match(/^\s*$/)) {
+        subs.push(last);
+        expect = 'new';
+      } else last.text += l + '\r\n';
     }
-    sub.push(data);
-    // console.log(data);
-    console.log(sub);
-    var lines = sub.join('').split(/\r*\n/);
-    var subs = [];
-    // var expect = 'new', last;
-    var expect = 'new';
-    var last;
-    console.log(expect);
-    let counter = 0;
-    lines.forEach(function(l) {
-
-      console.log('line element is ', l);
-        if (expect == 'new') {
-            last = {id: parseInt(l, 10), text:''};
-            if (!isNaN(last.id)) expect = 'time';
-        } else if (expect == 'time') {
-            var twoTimes = l.split(/\s+-+>\s+/);
-            // console.log('log of twoTimes', twoTimes);
-            last.start = parseTime(twoTimes[0]);
-            last.end = parseTime(twoTimes[1]);
-            expect = 'text_end';
-        }
-        else {
-            if (l.match(/^\s*$/)) {
-                subs.push(last);
-                expect = 'new';
-            }
-            else last.text += l + '\r\n';
-        }
-    });
-    console.log(lines);
-    console.log(lines.length);
-    console.log(subs);
-    console.log(subs.length);
-
-  })
-  return subs
+  });
+  return subs;
 }
-function main() {
 
-  const subs = createWorkingFile(filename)
+function shiftSubs(subs) {
+
+}
+
+function main(filename, userInput) {
+  // let wStream = fs.createWriteStream(__dirname + '/output.srt');
+  let wStream = fs.createWriteStream(__dirname + '/'
+  + filename.replace(/\w+(?=\.)/g, filename.match(/\w+(?=\.)/g) + '_fixedOutput'));
+
+  const subs = createWorkingFile(filename);
+  const specs = parseUserInput(userInput); // should return two objects, { at: t_i, shift: s_i }
+  console.log(specs);
+  console.log(filename);
+  var shifter = createShifter(specs);
+  subs.forEach(function(sub) {
+    sub.start = shifter(sub.start);
+    sub.end = shifter(sub.end);
+  })
+
+  subs
+    .map(function(sub) {
+      return [sub.id, '\r\n', stringifyTime(sub.start), ' --> ', stringifyTime(sub.end), '\r\n', sub.text, '\r\n'].join('')
+    })
+    .forEach(function(str) {
+      wStream.write(str, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+        }
+      })
+    });
+
+
 
 }
 
 app.get('/', (req, res) => {
-  // console.log('whatsup')
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body);
   if (req.files) {
-    // console.log(req.files);
-    var file = req.files.subtitleFile
+    var file = req.files.subtitleFile;
     var filename = file.name;
-    console.log(filename);
     file.mv(__dirname + '/uploads/' + filename, function(err) {
 
-      main(filename);
-      // res.send('file uploaded')
+      main(filename, req.body);
     })
   }
 
